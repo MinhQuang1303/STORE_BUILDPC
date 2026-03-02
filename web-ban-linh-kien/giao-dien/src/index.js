ba7f1c5b055125d34ab5779ec08be4202208fd0c
Cao Van Thuong <caothuong153@.com>
2026-03-02 20:17:45 +0700

    update UI layout

--- web-ban-linh-kien/giao-dien/src/index.js
+++ web-ban-linh-kien/giao-dien/src/index.js
@@ -1,7 +1,7 @@
-// File: web-ban-linh-kien/giao-dien/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
